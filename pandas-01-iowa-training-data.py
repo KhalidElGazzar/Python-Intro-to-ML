45e73c303bd17b81aacd3e36eba23d2499157783
Khalid ElGazzar <khalid.elgazzar@gmail.com>
2020-01-11 22:26:22 +0200

    Added dropna method to filter out missing in melb data. Added a comment about dropna method in the training data (iowa file).

--- pandas-01-iowa-training-data.py
+++ pandas-01-iowa-training-data.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
+print("Loading Training Data ..")
+
 # path of file to read
 iowa_file_path = './input/home_data/train.csv'
 
 # read the data and store it in a DataFrame
 iowa_data = pd.read_csv(iowa_file_path)
 
-print("Loading Training Data ..")
+'''
+if the data has some missing cells, then the simplist option for now is to drop
+the houses with missing data from the dataset
+We will use the dropna method where na means N/A
+'''
+# The iowa dataset (training data) is complete so there is no need to use dropna method
+# iowa_data = iowa_data.dropna(axis=0)
 
 # print columns
 print(iowa_data.columns)
--- pandas-02-melb-data.py
+++ pandas-02-melb-data.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
+print("Loading Melbourne Data ..")
+
 # path of file to read
 melb_file_path = './input/melbourne_data/melb_data.csv'
 
 # read the data and store it in a DataFrame
 melb_data = pd.read_csv(melb_file_path)
 
-print("Loading Melbourne Data ..")
+'''
+if the data has some missing cells, then the simplist option for now is to drop
+the houses with missing data from the dataset
+We will use the dropna method where na means N/A
+'''
+melb_data = melb_data.dropna(axis=0)
 
 # print columns
 print(melb_data.columns)
