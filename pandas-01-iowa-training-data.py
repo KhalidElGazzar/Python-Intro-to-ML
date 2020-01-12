65b0a7e9cf70ceaedf4525f28c0259d2903e955b
Khalid ElGazzar <khalid.elgazzar@gmail.com>
2020-01-12 14:39:12 +0200

    Added the 4 steps to build the model in both files (1. Define, 2. Fit, 3. Predict, 4. Evaluate)

--- pandas-01-iowa-training-data.py
+++ pandas-01-iowa-training-data.py
@@ -23,3 +23,61 @@ print(iowa_data.columns)
 print(iowa_data.describe())
 
 print("Data Loaded")
+
+print("Selecting Data for modeling")
+
+print ("Set the prediction target y (SalePrice)")
+# select the prediction target:
+# usually called y by convention
+# it is a single column stored in Series. A Series is a strcuture like DataFrame but with 
+# single column of data
+y = iowa_data.SalePrice
+
+print("Set the Features X: 'Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude' ")
+# select the Features - called X by convention
+# Features are the columns that are inputted in the model and will be used to determine
+# the prediction target y (price in our case)
+# Sometimes we use all columns except the prediction target as features.
+iowa_features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = iowa_data[iowa_features]
+
+# describe the Features
+print(X.describe())
+
+# show the first few (10) rows of the data
+# print(X.head(10))
+print(X.head())
+
+
+# Now, lets build the model
+'''
+We will use Scikit-learn library to create our model. This library is written as sklearn in our code
+It is easily the most popular library for modeling the types of data typically stores in DataFrames
+
+STEPS IN BUILDING & USING A MODEL
+1. Define:
+    - model type (Ex: DT or other type)
+    - model params
+2. Fit:
+    Capture patterns from provided data (This is the heart of modelling)
+3. Predict:
+    Crystal clear
+4. Evaluate:
+    Measure the accuracy of your model
+'''
+
+from sklearn.tree import DecisionTreeRegressor
+
+# define model: To ensure having same results each run, use random_state=1
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# fit model
+iowa_model.fit(X,y)
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(iowa_model.predict(X.head()))
+
+print("The actual prices are")
+print(iowa_data.SalePrice.head())
--- pandas-02-melb-data.py
+++ pandas-02-melb-data.py
@@ -45,5 +45,39 @@ print(X.describe())
 
 # show the first few (10) rows of the data
 # print(X.head(10))
-print(X.head(10))
+print(X.head())
+
+
+# Now, lets build the model
+'''
+We will use Scikit-learn library to create our model. This library is written as sklearn in our code
+It is easily the most popular library for modeling the types of data typically stores in DataFrames
+
+STEPS IN BUILDING & USING A MODEL
+1. Define:
+    - model type (Ex: DT or other type)
+    - model params
+2. Fit:
+    Capture patterns from provided data (This is the heart of modelling)
+3. Predict:
+    Crystal clear
+4. Evaluate:
+    Measure the accuracy of your model
+'''
+
+from sklearn.tree import DecisionTreeRegressor
+
+# define model: To ensure having same results each run, use random_state=1
+melb_model = DecisionTreeRegressor(random_state=1)
+
+# fit model
+melb_model.fit(X,y)
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melb_model.predict(X.head()))
+
+print("The actual prices are")
+print(melb_data.Price.head())
 
